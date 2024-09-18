5e9bc84d81360ba85ea1f18c6979e8c0e853c5b7
arturmagana <arturmagana@outlook.com>
2024-09-17 20:46:19 -0600

    Add files via upload

---
+++ ficheroBienvenida.js
@@ -0,0 +1,10 @@
+alert("Hola, Gracias por Visitar mi CV")
+var nombre  = prompt("Puedo saber tu Nombre?")
+if (nombre != ("no" || "No" || "Not" || "not")) {
+    var texto = "Hola " + nombre + " un gusto conocerlo"
+  }
+else{
+  var texto = "ok, saludos!"
+}
+alert(texto)
+console.log(texto)
